e249646859256fbab39554d13d4c1f41cb101917
auma2022 <auma.amwayi@student.moringaschool.com>
2022-06-10 22:52:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(driver, search) {
+  return driver.filter(
+    (theyMatch) => theyMatch.toLowerCase() === search.toLowerCase()
+  );
+}
+
+function fuzzyMatch(driver, output) {
+  return driver.filter(
+    (theyMatch) =>
+      theyMatch.toLowerCase().indexOf(output.toLowerCase()) === 0
+  );
+}
+
+function matchName(driver, search) {
+  return driver.filter((record) => record.name === search);
+}
